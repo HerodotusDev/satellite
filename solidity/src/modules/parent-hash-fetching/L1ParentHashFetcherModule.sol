0abf8db66b4602f0611e3dd789a73c44af382e71
Filip Krawczyk <kotmawszy@gmail.com>
2025-06-10 13:13:19 +0200

    Fix L1ParentHashFetcher

--- solidity/src/modules/parent-hash-fetching/L1ParentHashFetcherModule.sol
+++ solidity/src/modules/parent-hash-fetching/L1ParentHashFetcherModule.sol
@@ -3,11 +3,12 @@ pragma solidity ^0.8.27;
 
 import {ISatellite} from "../../interfaces/ISatellite.sol";
 import {IL1ParentHashFetcherModule} from "../../interfaces/modules/parent-hash-fetching/IL1ParentHashFetcherModule.sol";
+import {AccessController} from "../../libraries/AccessController.sol";
 
 /// @notice Fetches parent hashes for the L1 chain.
 /// @notice For example if deployed on Apechain, it will fetch parent hashes from Ethereum.
 /// @notice Works only on Arbitrum and Apechain, because they return L1 block hash from blockhash() function
-contract L1ParentHashFetcherModule is IL1ParentHashFetcherModule {
+contract L1ParentHashFetcherModule is IL1ParentHashFetcherModule, AccessController {
     bytes32 public constant KECCAK_HASHING_FUNCTION = keccak256("keccak");
 
     bytes32 constant MODULE_STORAGE_POSITION = keccak256("diamond.standard.satellite.module.storage.l1-parent-hash-fetcher");
@@ -28,7 +29,7 @@ contract L1ParentHashFetcherModule is IL1ParentHashFetcherModule {
     }
 
     /// @inheritdoc IL1ParentHashFetcherModule
-    function initL1ParentHashFetcherModule(uint256 l1ChainId) external {
+    function initL1ParentHashFetcherModule(uint256 l1ChainId) external onlyOwner {
         L1ParentHashFetcherModuleStorage storage s = moduleStorage();
         s.l1ChainId = l1ChainId;
     }

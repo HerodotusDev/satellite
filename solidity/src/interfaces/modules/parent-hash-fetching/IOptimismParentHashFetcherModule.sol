c44b585139e511fe55db5e3567beea38f0d37ad4
beeinger <sulecki.maciek@gmail.com>
2025-08-12 12:38:03 +0100

    initial implementation

---
+++ solidity/src/interfaces/modules/parent-hash-fetching/IOptimismParentHashFetcherModule.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.27;
+
+interface IDisputeGameFactory {
+    function gameAtIndex(uint256 _index) external view returns (uint32 gameType, uint64 timestamp, address proxy);
+}
+
+interface IFaultDisputeGame {
+    /// @notice Getter for the root claim of the dispute game.
+    /// @return The root claim.
+    function rootClaim() external view returns (bytes32);
+
+    /// @notice Returns the current status of the game.
+    /// @return The current game status.
+    function status() external view returns (uint8);
+
+    /// @notice Getter for the creator of the dispute game.
+    /// @return The address of the game creator.
+    function gameCreator() external view returns (address);
+}
+
+/// @notice Module that fetches the parent hash of blocks from Arbitrum
+/// @dev It needs to be deployed on the chain that Arbitrum settles on (L1)
+interface IOptimismParentHashFetcherModule {
+    struct OptimismParentHashFetcherModuleStorage {
+        // Chain ID of blocks being fetched
+        uint256 chainId;
+        // Dispute game factory
+        IDisputeGameFactory disputeGameFactory;
+        // Dispute game
+        address trustedGameProposer;
+    }
+
+    function initOptimismParentHashFetcherModule(uint256 chainId, address disputeGameFactory, address trustedGameProposer) external;
+
+    function optimismFetchParentHash(uint256 gameIndex, bytes32 versionByte, bytes32 stateRoot, bytes32 withdrawalStorageRoot, bytes memory blockHeader) external;
+}
---
+++ solidity/src/modules/parent-hash-fetching/OptimismParentHashFetcherModule.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.27;
+
+import {ISatellite} from "../../interfaces/ISatellite.sol";
+import {IOptimismParentHashFetcherModule, IDisputeGameFactory, IFaultDisputeGame} from "../../interfaces/modules/parent-hash-fetching/IOptimismParentHashFetcherModule.sol";
+import {AccessController} from "../../libraries/AccessController.sol";
+import {Lib_RLPReader as RLPReader} from "../../libraries/external/optimism/rlp/Lib_RLPReader.sol";
+
+// Mainnet: 0xe5965Ab5962eDc7477C8520243A95517CD252fA9
+
+/// @notice Fetches parent hashes for Optimism-like chains
+/// @notice if deployed on Ethereum Sepolia, it fetches parent hashes from Optimism Sepolia
+contract OptimismParentHashFetcherModule is IOptimismParentHashFetcherModule, AccessController {
+    using RLPReader for bytes;
+    using RLPReader for RLPReader.RLPItem;
+
+    bytes32 public constant KECCAK_HASHING_FUNCTION = keccak256("keccak");
+
+    // ========================= Satellite Module Storage ========================= //
+
+    bytes32 constant MODULE_STORAGE_POSITION = keccak256("diamond.standard.satellite.module.storage.optimism-parent-hash-fetcher");
+
+    function moduleStorage() internal pure returns (OptimismParentHashFetcherModuleStorage storage s) {
+        bytes32 position = MODULE_STORAGE_POSITION;
+        assembly {
+            s.slot := position
+        }
+    }
+
+    // ========================= Core Functions ========================= //
+
+    function initOptimismParentHashFetcherModule(uint256 chainId, address disputeGameFactory, address trustedGameProposer) external onlyOwner {
+        OptimismParentHashFetcherModuleStorage storage ms = moduleStorage();
+        ms.chainId = chainId;
+        ms.disputeGameFactory = IDisputeGameFactory(disputeGameFactory);
+        ms.trustedGameProposer = trustedGameProposer;
+    }
+
+    /// @param rootHash Root hash that is key of the mapping `roots` of `ERC20Outbox` contract
+    function optimismFetchParentHash(uint256 gameIndex, bytes32 versionByte, bytes32 stateRoot, bytes32 withdrawalStorageRoot, bytes memory blockHeader) external {
+        OptimismParentHashFetcherModuleStorage storage ms = moduleStorage();
+
+        (uint32 gameType, uint64 timestamp, address proxy) = ms.disputeGameFactory.gameAtIndex(gameIndex);
+        require(proxy != address(0), "ERR_GAME_NOT_FOUND");
+
+        IFaultDisputeGame game = IFaultDisputeGame(proxy);
+        uint8 status = game.status();
+
+        if (status == 1) {
+            revert("ERR_GAME_FAILED");
+        } else if (status == 0 && game.gameCreator() != ms.trustedGameProposer) {
+            revert("ERR_UNFINISHED_GAME_NOT_TRUSTED");
+        } else if (status != 2) {
+            revert("ERR_UNKNOWN_GAME_STATUS");
+        }
+
+        bytes32 rootClaim = game.rootClaim();
+
+        bytes32 blockHash = keccak256(blockHeader);
+        bytes32 payload = abi.encodePacked(stateRoot, withdrawalStorageRoot, blockHash);
+        bytes32 fullInput = abi.encodePacked(versionByte, payload);
+        bytes32 calculatedRoot = keccak256(fullInput);
+
+        require(rootClaim == calculatedRoot, "ERR_ROOT_CLAIM_MISMATCH");
+
+        uint256 blockNumber = _decodeBlockNumber(blockHeader);
+
+        ISatellite(address(this))._receiveParentHash(ms.chainId, KECCAK_HASHING_FUNCTION, blockNumber + 1, blockHash);
+    }
+
+    // ========================= Helper Functions ========================= //
+
+    function _decodeBlockNumber(bytes memory headerRlp) internal pure returns (uint256) {
+        return RLPReader.toRLPItem(headerRlp).readList()[8].readUint256();
+    }
+}
